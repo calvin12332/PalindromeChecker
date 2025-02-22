cea5a018771177fec71df3b7cd6bee0ce396f0d6
Calvin <Calvinrajbin2n@gmail.com>
2025-02-22 01:26:37 -0500

    sixth push

---
+++ Listjava.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import  java.util.List;
+
+     public class Listjava
+     {
+public static void main(String[] args) 
+  {
+     
+          {
+     
+               List < String > name =new ArrayList<>();
+               name.add("Ayo");
+               name.add("Homeless");
+               name.add("sravan");
+               name.add("narendra");
+               name.add("vamshi");
+               name.add("calvin");
+
+               System.out.println(name.get(4));
+          
+         };
+  }
+     }
